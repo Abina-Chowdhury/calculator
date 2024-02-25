fe1c1951007a9640972b99b9e08aedfdf3e05915
Abina Chowdhury <abina@etlimited.net>
2024-02-25 12:58:30 +0600

    create repo

---
+++ src/components/Calculate.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import { ref, reactive } from 'vue';
+import Display from "./Display.vue"
+
+const num1 = ref( "");
+const num2 = ref( "");
+
+
+</script>
+
+<template>
+         <div>
+          <h2>Calculator</h2>
+          <input type="number" v-model="num1">
+          <select v-model="operation">
+            <option>+</option>
+            <option>-</option>
+            <option>*</option>
+            <option>/</option>
+          </select>
+          <input type="number" v-model="num2">
+          <button type="submit">Calculate</button>
+          <p  >Result: {{ num1 }}</p>
+        </div>
+</template>
---
+++ src/components/Display.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+const QuoteDisplay = {
+      data() {
+        return {
+          quoteData: null
+        };
+      },
+      mounted() {
+        fetch('https://animechan.xyz/api/random')
+          .then(response => response.json())
+          .then(data => {
+            this.quoteData = data;
+          });
+      },
+    }
+</script>
+<template>
+    <div>
+          <h2>Random Anime Quote</h2>
+          <div v-if="quoteData">
+            <p><strong>Quote:</strong> {{ quoteData.quote }}</p>
+            <p><strong>Anime:</strong> {{ quoteData.anime }}</p>
+            <p><strong>Character:</strong> {{ quoteData.character }}</p>
+          </div>
+        </div>
+</template>
+
+<style>
+</style>
